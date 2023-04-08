1b74ca2f7fa3ccb02b0aeb23fd3350ad8d696105
Vinit Raut <vinit.coder@gmail.com>
2023-04-08 14:35:05 +0530

    Add: API logging for development

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,6 @@ import createServer from './server';
 const port = config.get<number>('port');
 const app = createServer();
 
-app.use('/', (req, res) => {
-  res.send('Hello World');
-});
-
 app.listen(port, async () => {
   logger.info(`Server started on port ${port}`);
 
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
+import config from 'config';
 import routes from './routes/routes';
 
 function createServer() {
   const app = express();
+
+  const nodeEnv = config.get<string>('nodeEnv');
+  if (nodeEnv == 'development') {
+    app.use(morgan('dev'));
+  }
 
   app.use(express.json());
 
