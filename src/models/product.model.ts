d57a87f1bb597d1bbea3005431cacdf86024de04
Vinit Raut <vinit.coder@gmail.com>
2023-04-16 13:57:35 +0530

    Update: mock data for products

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -25,7 +25,10 @@ const productSchema = new mongoose.Schema(
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    image: { type: String, required: true },
+    image: {
+      public_id: { type: String, required: true },
+      url: { type: String, required: true },
+    },
     countInStock: { type: Number, required: true },
     category: { type: String, required: true },
     brand: { type: String, required: true },
